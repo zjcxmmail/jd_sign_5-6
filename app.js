b6c1a1c377b4c91b5bf200c02fcbc221d203f94c
zjcxmmail <75290109+zjcxmmail@users.noreply.github.com>
2021-01-16 02:19:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ async function start() {
     let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
 
     
-    await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+    await sendNotify("京东签到5、6-" + "" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
   }
 }
 
