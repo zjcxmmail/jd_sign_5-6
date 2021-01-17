a4c19a02ebacc854ead455aae914f3710d4226c3
zjcxmmail <75290109+zjcxmmail@users.noreply.github.com>
2021-01-17 13:18:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ async function start() {
     return t_Date.Format('yyyyMMdd')
   }
     let text1 = dateFormat();     
-    await sendNotify("jd_sign_5_6京东签到_" + "" + ` ${res2} ` + ` ${res} ` +  ` ${text1} `, content); 
+    await sendNotify("jd_sign_5_6京东签到_" + "" + ` ${res2} ` + ` ${res} ` +  dateFormat(), content); 
 //  await sendNotify("jd_sign_5_6京东签到_" + "" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
   }
 }
