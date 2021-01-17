2b2b7c3d0fdd57457d8fac77c6ae466e5e3c4a4d
zjcxmmail <75290109+zjcxmmail@users.noreply.github.com>
2021-01-17 12:59:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ async function start() {
     var n_Date = new Date().getTime();
     var t_Date = new Date(n_Date + GMT_offset * 60 * 1000 + timezone * 60 * 60 * 1000);
     console.log(t_Date)
-    return t_Date.Format('yyyy.MM.dd')
+//    return t_Date.Format('yyyy.MM.dd')
   }
     let text = dateFormat();     
     await sendNotify("jd_sign_5_6京东签到_" + "" + ` ${res2} ` + ` ${res} ` + dateFormat(), content); 
